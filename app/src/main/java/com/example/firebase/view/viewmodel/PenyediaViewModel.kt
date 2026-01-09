ba2f044b61bd3687b47bf5bb0066a35b192eede0
Aulianurfitria15 <aulianurfitria17@gmail.com>
2026-01-09 18:11:04 +0700

    menambahkan initialisasi dari EditViewModel

--- app/src/main/java/com/example/firebase/view/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase/view/viewmodel/PenyediaViewModel.kt
@@ -17,5 +17,6 @@ object PenyediaViewModel {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { DetailViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EditViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa) }
     }
 }
